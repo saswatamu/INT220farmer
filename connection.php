d43043d584c902c9eda26a40bb96a6638f26537f
SASWATA 2001 <smukhopadhyay747@gmail.com>
2022-04-09 19:01:34 +0530

    first commit

---
+++ connection.php
@@ -0,0 +1,22 @@
+<?php
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+
+
+
+	$conn = new mysqli('localhost','root','','phpdbs');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into register(email, password) values(?, ?)");
+		$stmt->bind_param("ss", $email, $password);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+	
+?>
